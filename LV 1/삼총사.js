6a527a5e48309d61aa5eb259730327ce3acca260
khe <khj778878@naver.com>
2023-06-21 16:45:27 +0900

    [LV 1]: 삼총사_조합문제

---
+++ LV 1/삼총사.js
@@ -0,0 +1,44 @@
+// 첫번째 코드에서 문제에 필요한 조건만 해결하는 형태로 코드를 간결화시킴
+function solution(number) {
+  let cnt = 0;
+  function combinations(cb, i, x) {
+    if (x == 3) {
+      let sum = cb.reduce((a, b) => a + b);
+      if (sum === 0) cnt += 1;
+    } else {
+      for (let j = i; j < number.length; j++) {
+        cb.push(number[j]);
+        combinations([...cb], j + 1, x + 1);
+        cb.pop();
+      }
+    }
+  }
+
+  combinations([], 0, 0);
+  return cnt;
+}
+
+/** 첫 번째 코드 -> 3개를 고르는 조합을 전부 구하는 함수를 작성
+function solution(number) {
+  let comb = [];
+  let cnt = 0;
+  function combinations(cb, i, x) {
+    if (x == 3) {
+      comb.push(cb);
+    } else {
+      for (let j = i; j < number.length; j++) {
+        cb.push(number[j]);
+        combinations([...cb], j + 1, x + 1);
+        cb.pop();
+      }
+    }
+  }
+
+  combinations([], 0, 0);
+  comb.forEach((cb) => {
+    let sum = cb.reduce((a, b) => a + b);
+    if (sum === 0) cnt += 1;
+  });
+  return cnt;
+}
+ */
