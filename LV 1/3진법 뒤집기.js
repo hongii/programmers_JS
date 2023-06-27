13c13dc524e6b10936a7fc2deac8772b6a46f02b
khe <khj778878@naver.com>
2023-06-27 15:39:52 +0900

    [LV 1]: 3진법 뒤집기_js에서 진법 변환하는 방법

---
+++ LV 1/3진법 뒤집기.js
@@ -0,0 +1,29 @@
+function solution(n) {
+  let rem = "";
+  while (n > 0) {
+    rem += String(n % 3);
+    n = Math.floor(n / 3);
+  }
+  rem = [...rem].reverse();
+  let res = rem.reduce((acc, cur, i) => acc + parseInt(cur) * 3 ** i, 0);
+  return res;
+}
+
+/**
+ * toString() & parseInt() 의 조합 -> 진법 변형 가능
+ * 1) 10진수 -> n진수 문자열로 변형 : num.toString(n)
+ *   ex) let num = 8;
+ *       console.log(num.toString(2)); // 1000
+ *       console.log(num.toString(3)); // 22
+ *
+ * 2) n진수 문자열 -> 10진수 :
+ *   ex) let bin = "1011";
+ *       console.log(parseInt(bin, 2)); // 11
+ *   ex) let tri = "2122";
+ *       console.log(parseInt(tri, 3)); // 71
+ *
+ * 3) n진수 -> n진수
+ *   ex) parseInt("21121",3).toString(2); // 11001101
+ *                                        // "21121"이라는 3진수를 10진수로 바꾸고 이를 다시 2진수로 바꾼 값.
+ *                                        // 즉, 3진수 "21121"을 2진수로 변형하는 방법
+ */
