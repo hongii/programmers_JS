efe497e0702b500cef18c586aa3376652e89fb31
khe <khj778878@naver.com>
2023-07-15 16:05:29 +0900

    [LV 2]: k진수에서 소수 개수 구하기

---
+++ LV 2/k진수에서 소수 개수 구하기.js
@@ -0,0 +1,39 @@
+function solution(n, k) {
+  let convertNum = n.toString(k);
+  let tmp = "";
+  let cnt = 0;
+  [...convertNum.toString()].forEach((num, i) => {
+    if (num === "0") {
+      cnt = isPrime(+tmp) ? cnt + 1 : cnt;
+      tmp = "";
+    } else tmp += num;
+  });
+
+  if (tmp !== "") cnt = isPrime(+tmp) ? cnt + 1 : cnt;
+  return cnt;
+}
+
+function isPrime(num) {
+  if (num === 0 || num === 1) return false;
+  for (let i = 2; i <= +Math.sqrt(num); i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
+}
+
+/** 조금 더 간략한 다른사람 풀이
+function isPrime(num) {
+  if (num === 0 || num === 1) return false;
+  for (let i = 2; i <= +Math.sqrt(num); i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
+}
+
+function solution(n, k) {
+  // k진법으로 나눈 후 "0"을 기준으로 split
+  const candidates = n.toString(k).split("0");
+  // 소수 개수 세기
+  return candidates.filter((v) => isPrime(+v)).length;
+}
+ */
