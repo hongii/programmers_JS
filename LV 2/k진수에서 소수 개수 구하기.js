d03ff284b147d238464c1e7baffcce110c98878b
khe <khj778878@naver.com>
2023-07-15 16:06:40 +0900

    [LV 2]: k진수에서 소수 개수 구하기

--- LV 2/k진수에서 소수 개수 구하기.js
+++ LV 2/k진수에서 소수 개수 구하기.js
@@ -15,7 +15,7 @@ function solution(n, k) {
 
 function isPrime(num) {
   if (num === 0 || num === 1) return false;
-  for (let i = 2; i <= +Math.sqrt(num); i++) {
+  for (let i = 2; i <= Math.sqrt(num); i++) {
     if (num % i === 0) return false;
   }
   return true;
