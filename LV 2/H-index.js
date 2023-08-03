d2b5afd6a1ed0d2c9451725ccc8efb105d535ef2
khe <khj778878@naver.com>
2023-08-04 01:25:34 +0900

    [LV 2]: H-index_풀이 추가

--- LV 2/H-index.js
+++ LV 2/H-index.js
@@ -8,3 +8,15 @@ function solution(citations) {
     if (cnt >= H_index) return H_index;
   }
 }
+
+/* 더 효율적인 풀이 -> 시간 절약 */
+function solution(citations) {
+  citations = citations.sort((a, b) => b - a);
+  let i = 0;
+  while (i + 1 <= citations[i]) i++;
+  return i;
+}
+// * 코드 해설 *
+// 논문 인용횟수를 내림차순으로 정렬
+// 자신의 인용횟수(cittations[i])가 자신보다 인용횟수가 많은 논문 수(i) 보다 많으면 정답 수를 1 증가시킴
+// 자신의 인용횟수가 자신보다 인용횟수가 많은 논문 수와 같아 질때의 i를 return하면 정답
