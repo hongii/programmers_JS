4befe991145ede1bb41294f8c459a402f9470fb6
khe <khj778878@naver.com>
2023-10-22 03:18:38 +0900

    [LV 1]: 삼총사_조합(dfs)

--- LV 1/삼총사.js
+++ LV 1/삼총사.js
@@ -1,20 +1,20 @@
 // 첫번째 코드에서 문제에 필요한 조건만 해결하는 형태로 코드를 간결화시킴
 function solution(number) {
   let cnt = 0;
-  const combinations = (cb, i, x) => {
-    if (x == 3) {
+  const cb = [];
+  const combinations = (i) => {
+    if (cb.length == 3) {
       let sum = cb.reduce((a, b) => a + b);
       if (sum === 0) cnt += 1;
     } else {
       for (let j = i; j < number.length; j++) {
         cb.push(number[j]);
-        combinations([...cb], j + 1, x + 1);
+        combinations(j + 1);
         cb.pop();
       }
     }
   };
-
-  combinations([], 0, 0);
+  combinations(0);
   return cnt;
 }
 
